d8a0b8de61592972d2b4a438fadcde986b2706fc
Siusarna <42269105+Siusarna@users.noreply.github.com>
2019-07-27 16:25:02 +0300

    create simple landing

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendfile('index.html');
 });
 app.listen(port, () => {
   console.log(`Express web app available at localhost: ${port}`);
