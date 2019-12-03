fdc28f6838b1916d5a789078d0e35f20da72ac3d
Siusarna <42269105+Siusarna@users.noreply.github.com>
2019-12-04 00:49:07 +0200

    add feature reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 require('../models/index.js');
+const sha1 = require('sha1');
+const nodemailer = require('nodemailer');
+const config = require('../config/config.js');
 // const bCrypt = require('bcrypt');
 
 const authHelper = require('../helpers/authHelper.js');
@@ -47,9 +50,88 @@ const signUp = (req, res) => {
   }
 };
 
+const createTokenForResetPassword = () => {
+  const now = Date.now();
+  const token = sha1(now);
+  return token;
+};
+
+const createAndSendMessageToUserMail = async (email, token) => {
+  const optionsForSender = {
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: config.email, // generated ethereal user
+      pass: config.pass // generated ethereal password
+    }
+  };
+  const transporter = nodemailer.createTransport(optionsForSender);
+  const emailOptions = {
+    from: `"Siusarna 👻" ${config.email}`, // sender address
+    to: `${email}`, // list of receivers
+    subject: 'Reset Password ✔', // Subject line
+    text: `For reset your password enter this code: ${token}.\nIf you dont use reset password in SiusarnaSite ignore this letter` // plain text body
+  };
+  const info = await transporter.sendMail(emailOptions);
+  return info;
+};
+
+const forgot = (req, res) => {
+  const token = createTokenForResetPassword().slice(0, 10);
+  const { emailForResetPassword } = req.body;
+  User.findOneAndUpdate(
+    { email: emailForResetPassword },
+    { resetPasswordToken: token }
+  )
+    .exec()
+    .then((result) => {
+      createAndSendMessageToUserMail(emailForResetPassword, token);
+    })
+    .then((infoAboutEmail) => {
+      res.status(200).json('OK');
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json(error);
+    });
+};
+
+const checkTokenFromEmail = (req, res) => {
+  console.log(req.body);
+  const { email, token } = req.body;
+  User.findOne({ email })
+    .exec()
+    .then((result) => {
+      if (result.resetPasswordToken === token) {
+        res.status(200).json('OK');
+      } else {
+        res.status(401).json('Input token is not correctly');
+      }
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
+};
+
+const resetPassword = (req, res) => {
+  const { email, pass } = req.body;
+  User.findOneAndUpdate({ email }, { password: pass })
+    .exec()
+    .then((result) => {
+      res.status(200).json('Password successfully changed');
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json(error);
+    });
+};
 module.exports = {
   getLoginPage,
   getSignUpPage,
   signIn,
-  signUp
+  signUp,
+  forgot,
+  checkTokenFromEmail,
+  resetPassword
 };
--- route/auth.js
+++ route/auth.js
@@ -5,4 +5,7 @@ module.exports = (app) => {
   app.post('/login/', auth.signIn);
   app.get('/signUp/', auth.getSignUpPage);
   app.post('/signUp/', auth.signUp);
+  app.post('/login/forgot', auth.forgot);
+  app.post('/login/checkToken', auth.checkTokenFromEmail);
+  app.post('/login/resetPassword', auth.resetPassword);
 };
